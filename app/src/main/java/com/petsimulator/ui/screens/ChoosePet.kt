cc018385796c2b87d517c5894422e62a7700f9b0
Maria <mk902589@yandex.ru>
2025-01-13 22:38:50 +0700

    Добавила выбор вида питомца свайпами. Добавила выбор цвета

--- app/src/main/java/com/petsimulator/ui/screens/ChoosePet.kt
+++ app/src/main/java/com/petsimulator/ui/screens/ChoosePet.kt
@@ -1,11 +1,12 @@
 package com.petsimulator.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonColors
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,6 +26,7 @@ import com.petsimulator.model.Pet
 import com.petsimulator.model.Sex
 import com.petsimulator.utils.playSound
 import com.petsimulator.ui.GifAnimation
+import com.petsimulator.utils.GradientText
 import kotlin.reflect.KClass
 
 @Composable
@@ -43,59 +45,71 @@ fun ChoosePet(
         contentAlignment = Alignment.Center
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Box(modifier = Modifier.height(250.dp)) {
-                when (selectedPetType)  {
-                    Cat::class -> GifAnimation(R.drawable.choose_screen_cat)
-                    Dog::class -> GifAnimation(R.drawable.choose_screen_dog)
-                    Hamster::class -> GifAnimation(R.drawable.choose_screen_hamster)
-                    else -> GifAnimation(R.drawable.choose_screen_question, size = 200.dp)
-                }
-            }
-
             val petOptions = listOf(
                 Triple("Котик", Cat::class, R.raw.cat_choose_sound),
                 Triple("Собачка", Dog::class, R.raw.dog_choose_sound),
                 Triple("Хомяк", Hamster::class, R.raw.hamster_choose_sound)
             )
 
-            Spacer(modifier = Modifier.height(50.dp))
-            Text(text = "Выберите питомца:", style = MaterialTheme.typography.displaySmall)
-            Spacer(modifier = Modifier.height(16.dp))
+            val pagerState = rememberPagerState() { petOptions.size }
 
-            petOptions.forEach { (petClassName, petClass, soundRes) ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .selectable(
-                            selected = (selectedPetType == petClass),
-                            onClick = {
-                                selectedPetType = petClass as KClass<Pet>
-                                playSound(context, soundRes)
-                            }
-                        ),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    RadioButton(
-                        selected = (selectedPetType == petClass),
-                        onClick = {
-                            selectedPetType = petClass as KClass<Pet>
-                            playSound(context, soundRes)
-                        }
-                    )
-                    Text(text = petClassName)
-                }
+            val currentPage by remember { derivedStateOf { pagerState.currentPage } }
+
+            LaunchedEffect(currentPage) {
+                playSound(context, petOptions[currentPage].third)
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "Выберите пол питомца:", style = MaterialTheme.typography.displaySmall)
-            Spacer(modifier = Modifier.height(16.dp))
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp) //Увеличить высоту, чтобы разместить текст и гифку
+            ) { page ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally //Центровка по горизонтали
+                ) {
+                    //Текст над гифкой
+                    Text(
+                        text = "Ваш выбор: ${petOptions[page].first}",
+                        style = MaterialTheme.typography.displaySmall,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    //Гифка
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .weight(1f), //Растягиваем, чтобы текст и гифка не пересекались
+                        contentAlignment = Alignment.Center
+                    ) {
+                        GifAnimation(
+                            when (petOptions[page].second) {
+                                Cat::class -> R.drawable.choose_screen_cat
+                                Dog::class -> R.drawable.choose_screen_dog
+                                Hamster::class -> R.drawable.choose_screen_hamster
+                                else -> R.drawable.choose_screen_question
+                            }
+                        )
+                    }
+                }
+
+                selectedPetType = petOptions[page].second
+            }
 
             val sexOptions = listOf(
-                Triple("Мужской", Sex.MALE, Color.Blue to Color.LightGray),
-                Triple("Женский", Sex.FEMALE, pinkColor to Color.LightGray)
+                Sex.MALE to Color.Blue,
+                Sex.FEMALE to pinkColor
             )
 
-            sexOptions.forEach { (petSexName, petSex, petColor) ->
+            Spacer(modifier = Modifier.height(25.dp))
+            Text(text = "Выберите пол питомца:", style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(16.dp))
+
+            sexOptions.forEach { (petSex, petColor) ->
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -113,23 +127,60 @@ fun ChoosePet(
                             selectedPetSex = petSex
                         },
                         colors = RadioButtonDefaults.colors(
-                            selectedColor = petColor.first,
-                            unselectedColor = petColor.second
+                            selectedColor = petColor,
+                            unselectedColor = Color.LightGray
                         )
                     )
-                    Text(text = petSexName)
+                    Text(text = petSex.toString())
                 }
             }
             Spacer(modifier = Modifier.height(16.dp))
 
-            //Потом здесь написать логику
-            selectedPetColor = PetColor.getRandomColor()
+            Text(text = "Выберите цвет питомца:", style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(16.dp))
+
+            val colorOptions = listOf(
+                PetColor.RED to Color.Red,
+                PetColor.BLACK to Color.Black,
+                PetColor.WHITE to Color.White,
+                PetColor.MULTI to Color.Unspecified
+            )
+
+            colorOptions.forEach { (petColor, textColor) ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .selectable(
+                            selected = (selectedPetColor == petColor),
+                            onClick = {
+                                selectedPetColor = petColor
+                            }
+                        ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = (selectedPetColor == petColor),
+                        onClick = {
+                            selectedPetColor = petColor
+                        },
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = textColor,
+                            unselectedColor = Color.LightGray
+                        )
+                    )
+                    if (textColor == Color.Unspecified)
+                        GradientText(text = petColor.maleName)
+                    else
+                        Text(text = petColor.maleName)
+                }
+            }
 
             TextField(
                 value = selectedPetName,
                 onValueChange = { selectedPetName = it },
                 label = { Text("Имя питомца") },
-                placeholder = { Text("Просто имя вашего любимца...") }
+                placeholder = { Text("Просто имя вашего любимца...") },
+                singleLine = true
             )
             Spacer(modifier = Modifier.height(16.dp))
             Button(
